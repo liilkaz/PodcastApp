8c5eb99dde6daaa756e126245cc51cb6c5e12c96
Liliya (Tacarlen) <l.feodotova296@yandex.ru>
2023-09-25 17:55:24 +0300

    add extensions

---
+++ PodcastApp/Extensions/UILabel + Extension.swift
@@ -0,0 +1,16 @@
+//
+//  UILabel + Extension.swift
+//  PodcastApp
+//
+//  Created by Лилия Феодотова on 25.09.2023.
+//
+
+import UIKit
+
+extension UILabel {
+    convenience init(name: String, font: UIFont? = .jakarta16()) {
+        self.init()
+        self.text = name
+        self.font = font
+    }
+}
