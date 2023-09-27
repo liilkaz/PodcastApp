bcd9cd1dfaec7fc89258dc870bf4b6860ee28fff
Danila Bolshakov <47753945+DanilaBolshakov1999@users.noreply.github.com>
2023-09-27 20:37:30 +0300

    Update ProfileSettingViewController.swift

--- PodcastApp/ViewControllerTabBar/PresentationClasses/ProfileSettingVC.swift
+++ PodcastApp/ViewControllerTabBar/PresentationClasses/ProfileSettingVC.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ProfileSettingVC: UIViewController {
+class ProfileSettingViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
